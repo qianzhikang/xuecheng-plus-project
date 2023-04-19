1f3cfb1baac90f1cf162529f5387cc2695b5fed1
qianzhikang <qzkqianzhikang@gmail.com>
2023-04-19 08:22:13 +0800

    媒资管理 分片上传

---
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/api/MediaFilesController.java
@@ -0,0 +1,62 @@
+package com.xuecheng.media.api;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.dto.UploadFileParamsDto;
+import com.xuecheng.media.model.dto.UploadFileResultDto;
+import com.xuecheng.media.model.po.MediaFiles;
+import com.xuecheng.media.service.MediaFileService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.activation.MimeType;
+import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 媒资文件管理接口
+ * @date 2022/9/6 11:29
+ */
+@Api(value = "媒资文件管理接口", tags = "媒资文件管理接口")
+@RestController
+public class MediaFilesController {
+
+
+    @Resource
+    private MediaFileService mediaFileService;
+
+
+    @ApiOperation("媒资列表查询接口")
+    @PostMapping("/files")
+    public PageResult<MediaFiles> list(PageParams pageParams, @RequestBody QueryMediaParamsDto queryMediaParamsDto) {
+        Long companyId = 1232141425L;
+        return mediaFileService.queryMediaFiels(companyId, pageParams, queryMediaParamsDto);
+
+    }
+
+    @ApiOperation("上传图片")
+    @RequestMapping(value = "/upload/coursefile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public UploadFileResultDto upload(@RequestPart("filedata") MultipartFile multipartFile) throws IOException {
+        Long companyId = 1232141425L;
+        UploadFileParamsDto uploadFileParamsDto = new UploadFileParamsDto();
+        uploadFileParamsDto.setFileSize(multipartFile.getSize());
+        uploadFileParamsDto.setFilename(multipartFile.getOriginalFilename());
+        uploadFileParamsDto.setFileType("001001");
+        // 创建一个零时文件
+        File tempFile = File.createTempFile("minio", "temp");
+        multipartFile.transferTo(tempFile);
+        String absolutePath = tempFile.getAbsolutePath();
+        return mediaFileService.uploadFile(companyId,uploadFileParamsDto,absolutePath);
+    }
+
+
+
+}
---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
@@ -0,0 +1,40 @@
+package com.xuecheng.media.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.base.model.RestResponse;
+import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.dto.UploadFileParamsDto;
+import com.xuecheng.media.model.dto.UploadFileResultDto;
+import com.xuecheng.media.model.po.MediaFiles;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 媒资文件管理业务类
+ * @date 2022/9/10 8:55
+ */
+public interface MediaFileService {
+
+    /**
+     * 媒资文件查询方法
+     *
+     * @param pageParams          分页参数
+     * @param queryMediaParamsDto 查询条件
+     */
+    PageResult<MediaFiles> queryMediaFiels(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
+
+    MediaFiles addMediaFilesToDb(Long companyId, String fileMd5, UploadFileParamsDto uploadFileParamsDto, String bucket, String objectName);
+
+    UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto uploadFileParamsDto, String localFilePath);
+
+
+    RestResponse<Boolean> checkFile(String fileMd5);
+
+    RestResponse<Boolean> checkChunk(String fileMd5, int chunkIndex);
+
+    RestResponse uploadChunk(String fileMd5,int chunk,String localChunkFilePath);
+}
