3f1595e2937380a7fb13f9314011fd602f7ffbf0
qianzhikang <qzkqianzhikang@gmail.com>
2023-05-19 14:48:04 +0800

    finished

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -22,4 +22,12 @@ public interface CoursePublishService extends IService<CoursePublish> {
      * @param courseId  课程id
      */
     void commitAudit(Long companyId,Long courseId);
+
+
+    /**
+     * @description 课程发布接口
+     * @param companyId 机构id
+     * @param courseId 课程id
+     */
+    void publish(Long companyId,Long courseId);
 }
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CoursePublishServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CoursePublishServiceImpl.java
@@ -52,6 +52,9 @@ public class CoursePublishServiceImpl extends ServiceImpl<CoursePublishMapper, C
     @Resource
     CoursePublishPreMapper coursePublishPreMapper;
 
+    @Resource
+    CoursePublishMapper coursePublishMapper;
+
 
     /**
      * @param courseId 课程id
@@ -138,6 +141,85 @@ public class CoursePublishServiceImpl extends ServiceImpl<CoursePublishMapper, C
         courseBase.setAuditStatus("202003");
         courseBaseMapper.updateById(courseBase);
     }
+
+    @Transactional
+    @Override
+    public void publish(Long companyId, Long courseId) {
+
+        //约束校验
+        //查询课程预发布表
+        CoursePublishPre coursePublishPre = coursePublishPreMapper.selectById(courseId);
+        if(coursePublishPre == null){
+            XueChengPlusException.cast("请先提交课程审核，审核通过才可以发布");
+        }
+        //本机构只允许提交本机构的课程
+        if(!coursePublishPre.getCompanyId().equals(companyId)){
+            XueChengPlusException.cast("不允许提交其它机构的课程。");
+        }
+
+
+        //课程审核状态
+        String auditStatus = coursePublishPre.getStatus();
+        //审核通过方可发布
+        if(!"202004".equals(auditStatus)){
+            XueChengPlusException.cast("操作失败，课程审核通过方可发布。");
+        }
+
+        //保存课程发布信息
+        saveCoursePublish(courseId);
+
+        //保存消息表
+        saveCoursePublishMessage(courseId);
+
+        //删除课程预发布表对应记录
+        coursePublishPreMapper.deleteById(courseId);
+
+    }
+
+    /**
+     * @description 保存课程发布信息
+     * @param courseId  课程id
+     * @return void
+     * @author Mr.M
+     * @date 2022/9/20 16:32
+     */
+    private void saveCoursePublish(Long courseId){
+        //整合课程发布信息
+        //查询课程预发布表
+        CoursePublishPre coursePublishPre = coursePublishPreMapper.selectById(courseId);
+        if(coursePublishPre == null){
+            XueChengPlusException.cast("课程预发布数据为空");
+        }
+
+        CoursePublish coursePublish = new CoursePublish();
+
+        //拷贝到课程发布对象
+        BeanUtils.copyProperties(coursePublishPre,coursePublish);
+        coursePublish.setStatus("203002");
+        CoursePublish coursePublishUpdate = coursePublishMapper.selectById(courseId);
+        if(coursePublishUpdate == null){
+            coursePublishMapper.insert(coursePublish);
+        }else{
+            coursePublishMapper.updateById(coursePublish);
+        }
+        //更新课程基本表的发布状态
+        CourseBase courseBase = courseBaseMapper.selectById(courseId);
+        courseBase.setStatus("203002");
+        courseBaseMapper.updateById(courseBase);
+
+    }
+
+    /**
+     * @description 保存消息表记录
+     * @param courseId  课程id
+     * @return void
+     * @author Mr.M
+     * @date 2022/9/20 16:32
+     */
+    private void saveCoursePublishMessage(Long courseId){
+
+    }
+
 }
 
 
